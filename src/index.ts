f5c1c1712a32da353787374d736a0accdb3c537f
NorthIgor-dev <77532282+NorthIgor-dev@users.noreply.github.com>
2024-02-19 19:11:51 +0300

    modified

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { addOverLoad } from "./owerLoadedFunc";
 import { Counter } from "./owerloadClass";
 import { getTotal } from "./restParam";
 
-add().then(s=>console.log(s));
+add().then((s) => console.log(s));
 
-console.log(getDay(2020,4));
+console.log(getDay(2020, 4));
 
-console.log(getTotal(2,34,54,67));
+console.log(getTotal(2, 34, 54, 67));
 
-console.log(addOverLoad('e','r'));
+console.log(addOverLoad("e", "r"));
 
-const counter = new Counter;
+const counter = new Counter();
 console.log(counter.count(6));
