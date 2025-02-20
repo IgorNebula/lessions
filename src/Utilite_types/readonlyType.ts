8a0ed4b4e177901c1ed097998c9a6e30d59c19b7
NorthIgor-dev <77532282+NorthIgor-dev@users.noreply.github.com>
2025-02-20 17:21:46 +0300

    modified:   src/Utilite_types/readonlyType.ts

--- src/Utilite_types/readonlyType.ts
+++ src/Utilite_types/readonlyType.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * ### Readonly<Type>
  * Создает тип со всеми своими свойствами Type, доступными readonly,
  * что означает, что свойства созданного типа не могут быть переназначены.
  */
 
-interface Todo {
+interface ITodo {
   title: string;
 }
 
-const todo: Readonly<Todo> = {
+const todo: Readonly<ITodo> = {
   title: "Delete inactive users",
 };
 
-todo.title = "hello";
+//todo.title = "hello";//Не могу назначить «заголовок», потому что это свойство только для чтения.
